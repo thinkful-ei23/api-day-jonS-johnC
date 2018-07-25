ecdd162cf374133604ffc7441a81fc3158e1b5d3
jonsung89 <jonsung89@gmail.com>
2018-07-25 10:07:45 -0500

    completed 1. test an ajax call

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
-/* global shoppingList, store */
+'use strict';
+/* global $ shoppingList, store */
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
@@ -6,3 +7,8 @@ $(document).ready(function() {
 });
 
 store.items.push(Item.create('apples'));
+
+$.getJSON('https://thinkful-list-api.herokuapp.com/ei-student/items', response => {
+  console.log('api response:', response);
+});
+
